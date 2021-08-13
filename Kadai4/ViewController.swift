9ac4b26d1540b769a96767a4302560e0aa411197
吉田晃崇 <yoshidaakitaka@yoshidaakiratakashinoMacBook-Pro.local>
2021-08-13 14:55:09 +0900

    first commit

---
+++ Kadai4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kadai4
+//
+//  Created by 吉田晃崇 on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var count = 0
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    @IBAction func countUpButton(_ sender: Any){
+       count = count + 1
+       resultLabel.text = String(count)
+       }
+    
+    @IBAction func clearButton(_ sender: Any) {
+       count = 0
+       resultLabel.text = "0"
+       }
+}
+
